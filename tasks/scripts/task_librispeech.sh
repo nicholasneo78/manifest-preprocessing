c2a3c764d5f30fbe85f3c673e0e8f8d5f08dcf2c
nicholasneo78 <nicholasneo78@gmail.com>
2022-05-10 17:51:52 +0800

    config edits

--- tasks/scripts/task_librispeech.sh
+++ tasks/scripts/task_librispeech.sh
@@ -6,7 +6,9 @@ python3 ../task_generate_manifest.py \
     --dataset_name "librispeech" \
     --output_url "s3://experiment-logging" \
     --dataset_project "datasets/librispeech" \
-    --dataset_task_id "159b42223f4c46f89564ef51b251b2d2" \
-    --manifest_filename "manifest.json" \
-    --queue "cpu-only" \
+    --dataset_task_id "8efc2eb60e954b07857af7e9533f1639" \
+    --train_manifest_filename "train_manifest.json" \
+    --dev_manifest_filename "dev_manifest.json" \
+    --test_manifest_filename "test_manifest.json" \
+    --queue "compute" \
     --got_annotation \
--- tasks/task_generate_manifest.py
+++ tasks/task_generate_manifest.py
@@ -10,28 +10,28 @@ def parse_args():
     )
 
     # arguments corresponding to the task initialisation
-    parser.add_argument("--project_name",       type=str, help="the clearml project name")
-    parser.add_argument("--task_name",          type=str, help="clearml task name")
-    parser.add_argument("--dataset_name",       type=str, help="name of the output dataset produced")
-    parser.add_argument("--output_url",         type=str, help="the clearml url that the task will be output at")
-    parser.add_argument("--dataset_project",    type=str, help="the clearml path which the datasets resides")
-    parser.add_argument("--dataset_task_id",    type=str, help="the task id to retrieve the dataset")
-    parser.add_argument("--manifest_filename",  type=str, help="the manifest filename and directory to store it")
-    parser.add_argument("--got_annotation",     action='store_true', default=False, help="if annotation is needed in the .json file")
-    parser.add_argument("--queue",              type=str, help="the manifest filename and directory to store it")
+    parser.add_argument("--project_name",               type=str, help="the clearml project name")
+    parser.add_argument("--task_name",                  type=str, help="clearml task name")
+    parser.add_argument("--dataset_name",               type=str, help="name of the output dataset produced")
+    parser.add_argument("--output_url",                 type=str, help="the clearml url that the task will be output at")
+    parser.add_argument("--dataset_project",            type=str, help="the clearml path which the datasets resides")
+    
+    parser.add_argument("--dataset_task_id",            type=str, help="the task id to retrieve the dataset")
+    parser.add_argument("--train_manifest_filename",    type=str, help="the train manifest filename and directory to store it")
+    parser.add_argument("--dev_manifest_filename",      type=str, help="the dev manifest filename and directory to store it")
+    parser.add_argument("--test_manifest_filename",     type=str, help="the test manifest filename and directory to store it")
+    parser.add_argument("--got_annotation",             action='store_true', default=False, help="if annotation is needed in the .json file")
+    
+    parser.add_argument("--queue",                      type=str, help="the manifest filename and directory to store it")
 
     return parser.parse_args(sys.argv[1:])
 
 arg = parse_args()
 
-PROJECT_NAME = arg.project_name
-TASK_NAME = arg.task_name
-DATASET_NAME = arg.dataset_name
-OUTPUT_URL = arg.output_url
-DATASET_PROJECT = arg.dataset_project
+task = Task.init(project_name=arg.project_name,
+                 task_name=arg.task_name, 
+                 output_uri=arg.output_url)
 
-task = Task.init(project_name=PROJECT_NAME,
-                 task_name=TASK_NAME, output_uri=OUTPUT_URL)
 task.set_base_docker(
     docker_image="python:3.8.12-slim-buster",
     docker_setup_bash_script=[
@@ -43,14 +43,6 @@ task.set_base_docker(
     ]
 )
 
-args = {
-    'dataset_task_id': arg.dataset_task_id,
-    'manifest_filename': arg.manifest_filename,
-    'got_annotation': arg.got_annotation,
-}
-
-task.connect(args)
-
 # set to run
 task.execute_remotely(queue_name=arg.queue, exit_process=True)
 
@@ -58,23 +50,43 @@ from preprocessing.generate_manifest import GenerateManifest
 
 # register ClearML Dataset
 dataset = Dataset.create(
-    dataset_project=DATASET_PROJECT, dataset_name=DATASET_NAME, parent_datasets=[
-        args['dataset_task_id']]
+    dataset_project=arg.dataset_project, 
+    dataset_name=arg.dataset_name, 
+    parent_datasets=[arg.dataset_task_id]
 )
 
 dataset_path = dataset.get_local_copy()
 
-# process
-librispeech_manifest = GenerateManifest(
-    root_folder=dataset_path,
-    manifest_filename=args['manifest_filename'],
-    got_annotation=args['got_annotation']
-)
+# # process
+# librispeech_manifest = GenerateManifest(root_folder=dataset_path,
+#                                         manifest_filename=args['manifest_filename'],
+#                                         got_annotation=args['got_annotation']
+#                                         )
 
-new_manifest_path = librispeech_manifest()
+# new_manifest_path = librispeech_manifest()
 
-dataset.add_files(new_manifest_path)
-dataset.upload(output_url=OUTPUT_URL)
+train_manifest = GenerateManifest(root_folder=f'{dataset_path}/train',
+                                  manifest_filename=arg.train_manifest_filename,
+                                  got_annotation=arg.got_annotation)
+
+dev_manifest = GenerateManifest(root_folder=f'{dataset_path}/dev',
+                                manifest_filename=arg.dev_manifest_filename,
+                                got_annotation=arg.got_annotation)
+                                
+test_manifest = GenerateManifest(root_folder=f'{dataset_path}/test',
+                                 manifest_filename=arg.test_manifest_filename,
+                                 got_annotation=arg.got_annotation)
+
+train_manifest_path = train_manifest()
+dev_manifest_path = dev_manifest()
+test_manifest_path = test_manifest()
+
+# dataset.add_files(new_manifest_path)
+dataset.add_files(train_manifest_path)
+dataset.add_files(dev_manifest_path)
+dataset.add_files(test_manifest_path)
+
+dataset.upload(output_url=arg.output_url)
 dataset.finalize()
 
 print('Done')
